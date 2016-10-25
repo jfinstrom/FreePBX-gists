e2e147033ee732b55b8c0a7bacf79a6dc3ebdef9
James Finstrom <jfinstrom@gmail.com>
2016-10-25 06:22:12 -0700

    Update bs-form-input.php
    Remove extra divs to make xrobau happy

--- bs-form-input.php
+++ bs-form-input.php
@@ -7,17 +7,13 @@
 <!--NAME-->
 <div class="element-container">
 	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="form-group">
-					<div class="col-md-3">
-						<label class="control-label" for="name"><?php echo _("LABEL") ?></label>
-						<i class="fa fa-question-circle fpbx-help-icon" data-for="name"></i>
-					</div>
-					<div class="col-md-9">
-						<input type="text" class="form-control" id="name" name="name" value="">
-					</div>
-				</div>
+		<div class="form-group">
+			<div class="col-md-3">
+				<label class="control-label" for="name"><?php echo _("LABEL") ?></label>
+				<i class="fa fa-question-circle fpbx-help-icon" data-for="name"></i>
+			</div>
+			<div class="col-md-9">
+				<input type="text" class="form-control" id="name" name="name" value="">
 			</div>
 		</div>
 	</div>
