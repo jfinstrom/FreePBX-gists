fd39f56bd83acdc62cac3d0b1dc8b2ddf349df9b
James Finstrom <jfinstrom@gmail.com>
2017-08-17 18:10:08 -0700

    Fix the divs, do away with the 4/8 format

--- bs-page.php
+++ bs-page.php
@@ -10,19 +10,9 @@
 		<?php echo _('Page info')?>
 	</div>
 	<div class = "display full-border">
-		<div class="row">
-			<div class="col-sm-9">
-				<div class="fpbx-container">
-					<div class="display full-border">
-						Page contents
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-3 hidden-xs bootnav">
-				<div class="list-group">
-					<a href="?display=extensions" class="list-group-item active">Item One (active!)</a>
-					<a href="?display=extensions" class="list-group-item">Item two</a>
-				</div>
+		<div class="fpbx-container">
+			<div class="display full-border">
+				Page contents
 			</div>
 		</div>
 	</div>
