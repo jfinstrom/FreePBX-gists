05e5556c2a694eac86ab9d2639f4d2ffe2f6f786
James Finstrom <jfinstrom@gmail.com>
2017-08-17 18:12:41 -0700

    Remove the 4/8 format

--- bs-tabbed-page.php
+++ bs-tabbed-page.php
@@ -10,29 +10,20 @@
 		<?php echo _('Page info')?>
 	</div>
 	<div class = "display full-border">
-		<div class="row">
-			<div class="col-sm-9">
-				<div class="fpbx-container">
-					<div class="display full-border">
-						<ul class="nav nav-tabs" role="tablist">
-							<li data-name="tab1" class="change-tab active"><a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab">Tab1 (active!)</a></li>
-							<li data-name="tab2" class="change-tab"><a href="#tab2" aria-controls="tab2" role="tab" data-toggle="tab">Tab2</a></li>
-						</ul>
-						<div class="tab-content display">
-							<div id="tab1" class="tab-pane active">
-								Tab 1 Data (Shown)
-							</div>
-							<div id="tab2" class="tab-pane">
-								Tab 2 Data (Hidden)
-							</div>
-						</div>
+		<div class="fpbx-container">
+			<div class="display full-border">
+				<ul class="nav nav-tabs" role="tablist">
+					<li data-name="tab1" class="change-tab active"><a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab">Tab1 (active!)</a></li>
+					<li data-name="tab2" class="change-tab"><a href="#tab2" aria-controls="tab2" role="tab" data-toggle="tab">Tab2</a></li>
+				</ul>
+				<div class="tab-content display">
+					<div id="tab1" class="tab-pane active">
+						Tab 1 Data (Shown)
 					</div>
-				</div>
-			</div>
-			<div class="col-sm-3 hidden-xs bootnav">
-				<div class="list-group">
-					<a href="?display=extensions" class="list-group-item active">Item One (active!)</a>
-					<a href="?display=extensions" class="list-group-item">Item two</a>
+					<div id="tab2" class="tab-pane">
+						Tab 2 Data (Hidden)
+							</div>
+					</div>
 				</div>
 			</div>
 		</div>
