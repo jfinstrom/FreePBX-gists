e407afdc269cee65b73b22e629ed660bb919ff48
James Finstrom <jfinstrom@gmail.com>
2020-03-10 16:09:29 -0700

    code cleanup match php 5.6+

--- moduleclass.stub.php
+++ moduleclass.stub.php
@@ -1,5 +1,7 @@
 <?php
 namespace FreePBX\modules;
+use BMO;
+use FreePBX;
 /*
  * Class stub for BMO Module class
  * In _Construct you may remove the database line if you don't use it
@@ -8,71 +10,87 @@ namespace FreePBX\modules;
  *
  */
 
-class Classname implements \BMO {
-	public function __construct($freepbx = null) {
-		if ($freepbx == null) {
-			throw new Exception("Not given a FreePBX Object");
-		}
-		$this->FreePBX = $freepbx;
-		$this->db = $freepbx->Database;
-	}
-	public function install() {}
-	public function uninstall() {}
-	public function doConfigPageInit($page) {}
-	public function getActionBar($request) {
-		$buttons = array();
-		switch($_GET['display']) {
-			case 'modulename':
-				$buttons = array(
-					'delete' => array(
-						'name' => 'delete',
-						'id' => 'delete',
-						'value' => _('Delete')
-					),
-					'reset' => array(
-						'name' => 'reset',
-						'id' => 'reset',
-						'value' => _('Reset')
-					),
-					'submit' => array(
-						'name' => 'submit',
-						'id' => 'submit',
-						'value' => _('Submit')
-					)
-				);
-				if (empty($_GET['extdisplay'])) {
-					unset($buttons['delete']);
-				}
-			break;
-		}
-		return $buttons;
-	}
-	public function ajaxRequest($req, &$setting) {
-		switch ($req) {
-			case 'getJSON':
-				return true;
-			default:
-				return false;
-		}
-	}
-	public function ajaxHandler(){
-		switch ($_REQUEST['command']) {
-			case 'getJSON':
-				switch ($_GET['jdata']) {
-					case 'grid':
-						$ret = array();
-						/*code here to generate array*/
-						return $ret;
-					default:
-						return false;
-				}
-			default:
-				return false;
-		}
-	}
-	public function getRightNav($request) {
-		//You should probably load_view() here
-		$html = 'your custom html';
-		return $html;
-	}
+class Classname implements BMO
+{
+    public function __construct(FreePBX $freepbx)
+    {
+        $this->FreePBX = $freepbx;
+        $this->db      = $freepbx->Database;
+    }
+    
+    public function install()
+    {
+    }
+    
+    public function uninstall()
+    {
+    }
+    
+    public function doConfigPageInit($page)
+    {
+    }
+    
+    public function getActionBar($request)
+    {
+        $buttons = [];
+        switch ($_GET['display']) {
+            case 'modulename':
+                $buttons = [
+                    'delete' => [
+                        'name' => 'delete',
+                        'id' => 'delete',
+                        'value' => _('Delete')
+                    ],
+                    'reset' => [
+                        'name' => 'reset',
+                        'id' => 'reset',
+                        'value' => _('Reset')
+                    ],
+                    'submit' => [
+                        'name' => 'submit',
+                        'id' => 'submit',
+                        'value' => _('Submit')
+                    ],
+                ];
+                if (empty($_GET['extdisplay'])) {
+                    unset($buttons['delete']);
+                }
+                break;
+        }
+        return $buttons;
+    }
+
+    public function ajaxRequest($req, &$setting)
+    {
+        switch ($req) {
+            case 'getJSON':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public function ajaxHandler()
+    {
+        switch ($_REQUEST['command']) {
+            case 'getJSON':
+                switch ($_GET['jdata']) {
+                    case 'grid':
+                        $ret = [];
+                        /*code here to generate array*/
+                        return $ret;
+                    default:
+                        return false;
+                }
+            default:
+                return false;
+        }
+    }
+
+    public function getRightNav($request)
+    {
+        //You should probably load_view() here
+        $html = 'your custom html';
+        return $html;
+    }
 }
