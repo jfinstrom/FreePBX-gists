b1fb59abae9d2ab69d5085adbe71a2ac2dc241f1
James Finstrom <jfinstrom@gmail.com>
2021-04-27 10:55:04 -0700

    Update bs-form-input.php
    make find and replace friendly

--- bs-form-input.php
+++ bs-form-input.php
@@ -1,27 +1,27 @@
 <?php
 /*
- * Basic form input structure. Replace "name" with field id. Replace "Label" and "Help Text" appropriately
+ * Basic form input structure. Replace "__NAME__" with field id. Replace "__LABEL__" and "Help Text" appropriately
  */
 ?>
 
-<!--NAME-->
+<!--__LABEL__-->
 <div class="element-container">
 	<div class="row">
 		<div class="form-group">
 			<div class="col-md-3">
-				<label class="control-label" for="name"><?php echo _("LABEL") ?></label>
-				<i class="fa fa-question-circle fpbx-help-icon" data-for="name"></i>
+				<label class="control-label" for="name"><?php echo _("__LABEL__") ?></label>
+				<i class="fa fa-question-circle fpbx-help-icon" data-for="__NAME__"></i>
 			</div>
 			<div class="col-md-9">
-				<input type="text" class="form-control" id="name" name="name" value="">
+				<input type="text" class="form-control" id="__NAME__" name="__NAME__" value="">
 			</div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-12">
-			<span id="name-help" class="help-block fpbx-help-block"><?php echo _("Help Text")?></span>
+			<span id="__NAME__-help" class="help-block fpbx-help-block"><?php echo _("Help Text")?></span>
 		</div>
 	</div>
 </div>
-<!--END NAME-->
+<!--END __LABEL__-->
 
