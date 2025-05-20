2f1feeccd700c8e22b2c3582c2a997165a91d22d
James Finstrom <jfinstrom@gmail.com>
2025-05-19 17:37:09 -0700

    Update bs-tabbed-page.php bootstrap 4

--- bs-tabbed-page.php
+++ bs-tabbed-page.php
@@ -1,31 +1,30 @@
-<?php
-/*
- * Basic tabbed page
- */
-?>
-
 <div class="container-fluid">
-	<h1><?php echo _('Heading')?></h1>
-	<div class="well well-info">
-		<?php echo _('Page info')?>
-	</div>
-	<div class = "display full-border">
-		<div class="fpbx-container">
-			<div class="display full-border">
-				<ul class="nav nav-tabs" role="tablist">
-					<li data-name="tab1" class="change-tab active"><a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab">Tab1 (active!)</a></li>
-					<li data-name="tab2" class="change-tab"><a href="#tab2" aria-controls="tab2" role="tab" data-toggle="tab">Tab2</a></li>
-				</ul>
-				<div class="tab-content display">
-					<div id="tab1" class="tab-pane active">
-						Tab 1 Data (Shown)
-					</div>
-					<div id="tab2" class="tab-pane">
-						Tab 2 Data (Hidden)
-							</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <h1><?php echo _('Heading')?></h1>
+    <div class="alert alert-info" role="alert">
+        <?php echo _('Page info')?>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-12">
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="tab1-tab" data-toggle="tab" href="#tab1" role="tab" aria-controls="tab1" aria-selected="true">Tab1 (active!)</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="tab2-tab" data-toggle="tab" href="#tab2" role="tab" aria-controls="tab2" aria-selected="false">Tab2</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content pt-3">
+                        <div class="tab-pane fade show active" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">
+                            Tab 1 Data (Shown)
+                        </div>
+                        <div class="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">
+                            Tab 2 Data (Hidden)
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
